86b3d497d6a7165ca68598729ca85dc41cd9e9b4
BuildTools <unconfigured@null.spigotmc.org>
2025-10-11 22:24:03 -0400

    bumping

--- Assets/Scripts/Gameplay/Slingshot.cs
+++ Assets/Scripts/Gameplay/Slingshot.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 
 public class Slingshot : Weapon
@@ -17,6 +18,7 @@ public class Slingshot : Weapon
         if (Input.GetMouseButtonDown(0))
         {
             EventBus.Publish(new WeaponTriggerEvent("Slingshot"));
+            
         }
 
     }
@@ -25,5 +27,22 @@ public class Slingshot : Weapon
     {
         Debug.Log("Slingshot fired!");
         // Implement slingshot-specific behavior here
+        //Debug.Log("mouseDown = " + Input.mousePosition.x + " " + Input.mousePosition.y);
+        //Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+        //Vector3 dir = (worldPos - transform.position).normalized;
+        //Debug.Log("worldPos = " + worldPos + " dir = " + dir);
+        
+        
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float distance;
+        Vector3 relativeMousePosition = Vector3.zero;
+        if (playerPlane.Raycast(ray, out distance))
+        {
+            relativeMousePosition = ray.GetPoint(distance);
+        }
+        Debug.Log("relativeMousePosition = " + (relativeMousePosition - GameManager.Instance.GetPlayer().transform.position));
+        
+        EventBus.Publish(new PlayerBumpEvent((GameManager.Instance.GetPlayer().transform.position - relativeMousePosition), 40f));
     }
 }
