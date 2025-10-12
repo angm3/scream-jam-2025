a6420042ddd14ab554c206c72a305c6d9f3eaed2
Sal Aslam <aslam.sal.eng@gmail.com>
2025-10-12 18:52:20 -0400

    ss sm

--- Assets/Scripts/Gameplay/Slingshot.cs
+++ Assets/Scripts/Gameplay/Slingshot.cs
@@ -1,58 +1,105 @@
 
 using UnityEngine;
 
-public class Slingshot : Weapon
+
+public class Slingshot : MonoBehaviour
 {
-
+    public StateMachine<Slingshot> stateMachine;
     public GameObject projectilePrefab;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
 
+    public Slingshot()
+    {
+        stateMachine = new StateMachine<Slingshot>();
+        stateMachine.ChangeState(new SlingshotCanShootState(this, stateMachine));
+    }
+    
+    void Update()
+    {
+        stateMachine.Update();
+    }
+}
+
+
+public class SlingshotCanShootState : State<Slingshot>
+{
+    public SlingshotCanShootState(Slingshot slingshot, StateMachine<Slingshot> stateMachine) : base(slingshot, stateMachine) { }
+
+    public override void Enter()
+    {
+        Debug.Log("Slingshot is ready to shoot");
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            EventBus.Publish(new WeaponTriggerEvent("Slingshot"));
-            
+            owner.stateMachine.ChangeState(new SlingshotShootingState(owner, stateMachine));
         }
     }
+}
 
-    public override void TriggerWeapon(WeaponTriggerEvent e)
+public class SlingshotShootingState : State<Slingshot>
+{
+    public SlingshotShootingState(Slingshot slingshot, StateMachine<Slingshot> stateMachine) : base(slingshot, stateMachine) { }
+
+
+    public float max_hold_timer = 1.5f;
+    public float hold_for_timer;
+    public override void Enter()
     {
-        Debug.Log("Slingshot fired!");        
-        
-        Plane playerPlane = new Plane(Vector3.up, transform.position);
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        float distance;
-        Vector3 relativeMousePosition = Vector3.zero;
-        if (playerPlane.Raycast(ray, out distance))
-        {
-            relativeMousePosition = ray.GetPoint(distance);
-        }
-        Debug.Log("relativeMousePosition = " + (relativeMousePosition - GameManager.Instance.GetPlayer().transform.position));
+        hold_for_timer = 0f;
+    }
 
-        //EventBus.Publish(new PlayerBumpEvent((GameManager.Instance.GetPlayer().transform.position - relativeMousePosition), 40f));
+    public override void Update()
+    {
+        hold_for_timer += Time.deltaTime;
 
-        /*
-        Vector3 bump_dir = Vector3.zero;
-        if (relativeMousePosition.z > 0)
+        if (Input.GetMouseButtonUp(0))
         {
-            bump_dir = new Vector3(0, 0, -1f);
-        }
-        else if (relativeMousePosition.z < 0)
-        {
-            bump_dir = new Vector3(0, 0, 1f);
-        }
-        EventBus.Publish(new PlayerBumpEvent(bump_dir, 40f));
-        */
+            // Shoot
+            Plane playerPlane = new Plane(Vector3.up, owner.transform.position);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float distance;
+            Vector3 relativeMousePosition = Vector3.zero;
+            if (playerPlane.Raycast(ray, out distance))
+            {
+                relativeMousePosition = ray.GetPoint(distance);
+            }
+            Debug.Log("relativeMousePosition = " + (relativeMousePosition - GameManager.Instance.GetPlayer().transform.position));
 
-        Vector3 baes_projectile_dir = -(GameManager.Instance.GetPlayer().transform.position - relativeMousePosition).normalized;
-        GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
-        projectile.GetComponent<Rigidbody>().linearVelocity = baes_projectile_dir * 30f;
+            Vector3 baes_projectile_dir = -(GameManager.Instance.GetPlayer().transform.position - relativeMousePosition).normalized;
+            // Create projectile
+            //GameObject projectilePrefab = Resources.Load<GameObject>("Prefabs/Projectile");
+            GameObject projectile = GameObject.Instantiate(owner.projectilePrefab, owner.transform.position, Quaternion.identity);
+            projectile.GetComponent<Rigidbody>().linearVelocity = baes_projectile_dir * Mathf.Min(30f, 5f + hold_for_timer * 20f);
+
+            // Recoil player
+            //EventBus.Publish(new PlayerBumpEvent(-baes_projectile_dir, Mathf.Min(20f, 5f + hold_for_timer * 15f)));
+
+            owner.stateMachine.ChangeState(new SlingshotCooldownState(owner, stateMachine));
+        }
+    }
+}
+
+
+
+
+public class SlingshotCooldownState : State<Slingshot>
+{
+    public SlingshotCooldownState(Slingshot slingshot, StateMachine<Slingshot> stateMachine) : base(slingshot, stateMachine) { }
+    public float cooldown_timer = 0.75f;
+    public float cooldown_time_counter;
+    public override void Enter()
+    {
+        cooldown_time_counter = 0f;
+    }
+    
+    public override void Update()
+    {
+        cooldown_time_counter += Time.deltaTime;
+        if (cooldown_time_counter >= cooldown_timer)
+        {
+            owner.stateMachine.ChangeState(new SlingshotCanShootState(owner, stateMachine));
+        }
     }
 }
