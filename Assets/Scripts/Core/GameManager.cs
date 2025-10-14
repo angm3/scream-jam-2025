209fd9b552de33d54f563fc359032e8ef913000f
Tristan Klintworth <frostflag@gmail.com>
2025-10-14 02:57:17 -0400

    bunch of garage and stash logic

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }
 
     public Stash stash;
+    public Inventory currentPlayerInventory;
 
     //private Transform playerTransform;
 
@@ -22,7 +23,9 @@ public class GameManager : MonoBehaviour
             Instance = this;
             DontDestroyOnLoad(gameObject); // Persist across scenes
 
+            // Set up stash and inventory 
             stash = new Stash();
+            currentPlayerInventory = new Inventory();
         }
     }
 
@@ -30,7 +33,7 @@ public class GameManager : MonoBehaviour
     {
         Debug.Log("Extraction Successful");
         // TODO: Transfer inventory items to "stash"
-        TransferInventoryToStash();
+        //TransferInventoryToStash();
         ReturnToGarage();
     }
     
--- Assets/Scripts/Core/GarageManager.cs
+++ Assets/Scripts/Core/GarageManager.cs
@@ -6,10 +6,20 @@ public class GarageManager : MonoBehaviour
 {
     [Header("UI Elements")]
     [SerializeField] private Button startRunButton;
-    [SerializeField] private TextMeshProUGUI counts;
 
-    // Inventory UI
-    // Stash UI
+    // Inventory and Stash UI
+    [Header("Stash/Inventory Grids")]
+    public GameObject inventorySlotItemPrefab;
+    public Transform inventoryGridContainer;
+    public Transform stashGridContainer;
+
+    [Header("Sprites")]
+    public Sprite candySprite;
+
+    [Header("Properties")]
+    [SerializeField] public int inventorySize = 8;
+    [SerializeField] public int stashSize = 24;
+    
 
     // Handle creating the appropriate UI from Stash and Inventory
 
@@ -18,31 +28,83 @@ public class GarageManager : MonoBehaviour
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
 
+        //Stash stash = GameManager.Instance.stash;
+        //Inventory inventory = GameManager.Instance.currentPlayerInventory;
+
+        CreateEmptySlots(inventoryGridContainer, inventorySize);
+        CreateEmptySlots(stashGridContainer, stashSize);
+
+        LoadPlayerInventory();
+        LoadStash();
+    }
+
+    void LoadPlayerInventory()
+    {
+        Inventory playerInv = GameManager.Instance.currentPlayerInventory;
+
+        if (playerInv == null) 
+        {
+            Debug.LogWarning("No inventory in gamemanager");
+        }
+
+        Debug.Log($"Loading inventory with {playerInv.candyCount} candy");
+
+        if (playerInv.candyCount > 0)
+        {
+            CreateUIItem(inventoryGridContainer, "candy", "", playerInv.candyCount, candySprite);
+        }
+
+        // TODO items and ingredients
+    }
+
+    void LoadStash()
+    {
         Stash stash = GameManager.Instance.stash;
 
-        Debug.Log($"Stash has {stash.candyCount} candy");
-        counts.text = "Candy: " + stash.candyCount;
+        if (stash == null) return;
+
+        if (stash.candyCount > 0)
+        {
+            CreateUIItem(stashGridContainer, "candy", "", stash.candyCount, candySprite);
+        }
+
+        // TODO items and ingredients
+    }
+
+    void CreateUIItem(Transform parent, string type, string id, int count, Sprite sprite)
+    {
+        Debug.Log("Attempting to create a UI Element");
+        GameObject itemUI = Instantiate(inventorySlotItemPrefab, parent);
+
+        CollectibleUIItem item = itemUI.GetComponent<CollectibleUIItem>();
+        item.collectibleType = type;
+        item.collectibleId = id;
+        item.stackCount = count;
+        item.iconImage.sprite = sprite;
+
+        if (item.stackText != null)
+        {
+            item.stackText.gameObject.SetActive(count > 1);
+            item.stackText.text = count.ToString();
+        }
+    }
+
+    void CreateEmptySlots(Transform grid, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            GameObject slot = Instantiate(inventorySlotItemPrefab, grid);
+            if (slot.GetComponent<CollectibleUIItem>().stackText != null) {
+                slot.GetComponent<CollectibleUIItem>().stackText.gameObject.SetActive(false);
+            }
+        }
     }
 
     public void OnStartRunClicked()
     {
         Debug.Log("Button clicked");
-
-        // Tell GameManager to prepare new run
         GameManager.Instance.StartNewRun();
-
-        // Load neighborhood scene
         SceneController.Instance.LoadScene("SampleScene", includeUI: true);
-    }
-
-    public void DrawStash()
-    { 
-        // Draw all the appropriate sprites based on what is in the stash
-    }
-
-    public void DrawInventory()
-    { 
-        // Draw all the appropriate sprites based on what is in the inventory
     }
 }
 
